e0d3361e7054bcba75a8b64b8e7d0526334c049e
CJ Lucido <c.jose.lucido@gmail.com>
2019-11-04 13:55:51 -0800

    changed ProductContext object values to better represent the information being passed in

--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -2,11 +2,9 @@ import { createContext } from 'react'
 
 
 const ProductContext = createContext({
-    //unsure that this is the structure I'm actually putting here, might just be putting an array of objects commenting out until sure (this is just for documentation clarity anyway)
-    // id: "",
-    // title: "",
-    // price: "",
-    // image: ""
+    products: {},
+
+    addItem: () => {}
 })
 
 export default ProductContext;
