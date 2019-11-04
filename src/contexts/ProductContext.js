f2a87238716e5bf293c7f4b4bff002ede31d39cd
CJ Lucido <c.jose.lucido@gmail.com>
2019-11-04 13:29:06 -0800

    created a ProductContext, there is a note for me to improve documentation when I am sure what form I want the context structure to accept

--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -1,1 +1,12 @@
 import { createContext } from 'react'
+
+
+const ProductContext = createContext({
+    //unsure that this is the structure I'm actually putting here, might just be putting an array of objects commenting out until sure (this is just for documentation clarity anyway)
+    // id: "",
+    // title: "",
+    // price: "",
+    // image: ""
+})
+
+export default ProductContext;
