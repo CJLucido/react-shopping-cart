2753a6195321afb5a62523bdf121ccc051a746c5
CJ Lucido <c.jose.lucido@gmail.com>
2019-11-04 14:04:19 -0800

    wrapped navigation and routes in CartContext after creating CartContext

---
+++ src/contexts/CartContext.js
@@ -0,0 +1,7 @@
+import {createContext} from 'react'
+
+export const CartContext = createContext({
+
+//aha - the object here will be comprised of the object set on the value prop of the provider component!
+
+})
