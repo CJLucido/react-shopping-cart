804093e55e74143a6eb475f5b1c403693911d4ee
CJ Lucido <c.jose.lucido@gmail.com>
2019-11-04 13:53:55 -0800

    refactored Products to get data from the Products Context and not props

--- src/components/Products.js
+++ src/components/Products.js
@@ -6,14 +6,16 @@ import Product from './Product';
 //Context
 import ProductContext from "../contexts/ProductContext"
 
-const Products = props => {
+const Products = () => {
+	const {products, addItem} = useContext(ProductContext)
+
 	return (
 		<div className="products-container">
-			{props.products.map(product => (
+			{products.map(product => (
 				<Product
 					key={product.id}
 					product={product}
-					addItem={props.addItem}
+					addItem={addItem}
 				/>
 			))}
 		</div>
